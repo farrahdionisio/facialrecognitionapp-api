5711a0f8c50ae74c849925dd1597390b2fd63a81
farrahdionisio <farrahdsdionisio@gmail.com>
2021-08-03 09:22:58 +0800

    changing response

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(express.json());
 
 app.use(cors())
 
-// app.get('/', (req, res) => {
-//     res.send(database.users);
-// })
+app.get('/', (req, res) => {
+    res.send('it is working');
+})
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
